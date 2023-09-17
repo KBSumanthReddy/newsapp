cb814dfdb41459a53582dda66cba25de1b76b1b7
KBSumanthReddy <117896310+KBSumanthReddy@users.noreply.github.com>
2023-09-17 20:17:20 +0530

    deploy commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
+const NewsItem =(props)=> {
     return (
       <div className="my-3">
          <div className="card">
           <div style={{display:'flex', justifyContent:'flex-end', position: 'absolute', right:0}}>
-            <span className='badge rounded-pill bg-danger'>{source}</span>
+            <span className='badge rounded-pill bg-danger'>{props.source}</span>
           </div>
-          <img src={!imageUrl?"https://cdn.dribbble.com/users/6238/screenshots/3213019/media/d7dc83d654069a3850683783445ae58f.jpg?resize=400x300&vertical=center":imageUrl} className="card-img-top" alt="..."/>
+          <img src={!props.imageUrl?"https://cdn.dribbble.com/users/6238/screenshots/3213019/media/d7dc83d654069a3850683783445ae58f.jpg?resize=400x300&vertical=center":props.imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className='card-text'><small className='text-muted'>By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
-            <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
+            <h5 className="card-title">{props.title}...</h5>
+            <p className="card-text">{props.description}...</p>
+            <p className='card-text'><small className='text-muted'>By {!props.author?"Unknown":props.author} on {new Date(props.date).toGMTString()}</small></p>
+            <a rel='noreferrer' href={props.newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
           </div>
         </div>
       </div>
     )
-  }
 }
 
 export default NewsItem
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import loading from './Loading.gif'
 
-export class spinner extends Component {
-  render() {
+const spinner=()=> {
     return (
       <div className='text-center'>
         <img className='my-3' src={loading} alt="loading" style={{height:'50px',width:'50px'}} />
       </div>
     )
-  }
 }
 
 export default spinner
